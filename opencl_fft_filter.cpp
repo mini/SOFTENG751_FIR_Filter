8abc316be877611f48c6e59301626a8ba0448066
Bradley Coleman <bcol085@UoA.auckland.ac.nz>
2020-06-12 01:01:25 +1200

    Made fft filter work to an extent. Currently very wrong/different from other filters.

--- opencl_fft_filter.cpp
+++ opencl_fft_filter.cpp
@@ -3,36 +3,28 @@
 
 
 void filter::OpenCLFFT::doFilter(float* input, uint64_t inputLength, float* weights, uint64_t weightsLength, float* output) {
-	uint64_t FFT_size = 2^(int)(ceil(log(inputLength + weightsLength - 1)/log(2)));
+	uint64_t FFT_size = pow(2, (ceil(log(inputLength + weightsLength - 1) / log(2))));
 	clfftPlanHandle planHandle;
 	clfftDim dim = CLFFT_1D;
 	size_t clLengths[1] = { FFT_size };
 
-	float* paddedInput = (float*)malloc(FFT_size * sizeof(float));
-	memcpy(paddedInput, input, sizeof(input));
-	memset(paddedInput + sizeof(input), 0, (FFT_size - inputLength) * sizeof(float));
-
-	float* paddedWeights = (float*)malloc(FFT_size * sizeof(float));
-	memcpy(paddedWeights, weights, sizeof(weights));
-	memset(paddedWeights + sizeof(weights), 0, (FFT_size - weightsLength) * sizeof(float));
-
-	float* freqInput = (float*)calloc(FFT_size, sizeof(float));
-	float* freqOutput = (float*)calloc(FFT_size, sizeof(float));
-	float* freqWeights = (float*)calloc(FFT_size, sizeof(float));
+	float* freqInput = (float*)calloc(inputLength, sizeof(float));
+	float* freqOutput = (float*)calloc(inputLength, sizeof(float));
+	float* freqWeights = (float*)calloc(weightsLength, sizeof(float));
 
 	/* Setup clFFT. */
 	clfftSetupData fftSetup;
 	err = clfftInitSetupData(&fftSetup);
 	err = clfftSetup(&fftSetup);
 
-	cl_mem timeInputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, FFT_size * sizeof(float), paddedInput, &err);
+	cl_mem timeInputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputLength * sizeof(float), input, &err);
 	checkError("clCreateBuffer 0");
-	cl_mem freqInputBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE, FFT_size * sizeof(float), freqInput, &err);
+	cl_mem freqInputBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE, inputLength * sizeof(float), NULL, &err);
 	checkError("clCreateBuffer 0");
 
-	cl_mem weightsBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, FFT_size * sizeof(float), paddedWeights, &err);
+	cl_mem weightsBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, weightsLength * sizeof(float), weights, &err);
 	checkError("clCreateBuffer 0");
-	cl_mem freqWeightsBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE, FFT_size * sizeof(float), freqWeights, &err);
+	cl_mem freqWeightsBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE, weightsLength * sizeof(float), NULL, &err);
 	checkError("clCreateBuffer 0");
 
 	err = clfftCreateDefaultPlan(&planHandle, context, dim, clLengths);
@@ -41,22 +33,28 @@ void filter::OpenCLFFT::doFilter(float* input, uint64_t inputLength, float* weig
 	err = clfftSetResultLocation(planHandle, CLFFT_OUTOFPLACE);
 	err = clfftBakePlan(planHandle, 1, &command_queue, NULL, NULL);
 
-	err = clEnqueueWriteBuffer(command_queue, timeInputBuffer, CL_TRUE, 0, FFT_size * sizeof(float), paddedInput, 0, NULL, NULL);
+	err = clEnqueueWriteBuffer(command_queue, timeInputBuffer, CL_TRUE, 0, inputLength * sizeof(float), input, 0, NULL, NULL);
 	err = clfftEnqueueTransform(planHandle, CLFFT_FORWARD, 1, &command_queue, 0, NULL, NULL, &timeInputBuffer, &freqInputBuffer, NULL);
 	err = clFinish(command_queue);
-	err = clEnqueueReadBuffer(command_queue, freqInputBuffer, CL_TRUE, 0, FFT_size * sizeof(float), freqInput, 0, NULL, NULL);
+	err = clEnqueueReadBuffer(command_queue, freqInputBuffer, CL_TRUE, 0, inputLength * sizeof(float), freqInput, 0, NULL, NULL);
 
-	err = clEnqueueWriteBuffer(command_queue, weightsBuffer, CL_TRUE, 0, FFT_size * sizeof(float), paddedWeights, 0, NULL, NULL);
+	err = clEnqueueWriteBuffer(command_queue, weightsBuffer, CL_TRUE, 0, weightsLength * sizeof(float), weights, 0, NULL, NULL);
 	err = clfftEnqueueTransform(planHandle, CLFFT_FORWARD, 1, &command_queue, 0, NULL, NULL, &weightsBuffer, &freqWeightsBuffer, NULL);
 	err = clFinish(command_queue);
-	err = clEnqueueReadBuffer(command_queue, freqWeightsBuffer, CL_TRUE, 0, FFT_size * sizeof(float), freqWeights, 0, NULL, NULL);
+	err = clEnqueueReadBuffer(command_queue, freqWeightsBuffer, CL_TRUE, 0, weightsLength * sizeof(float), freqWeights, 0, NULL, NULL);
+	
+	OpenCLTimeDomain::doFilter(freqInput, inputLength, freqWeights, weightsLength, output);
 
-	OpenCLTimeDomain::doFilter(freqInput, FFT_size, freqWeights, FFT_size, output);
+	err = clEnqueueWriteBuffer(command_queue, freqInputBuffer, CL_TRUE, 0, (inputLength + weightsLength) * sizeof(float), output, 0, NULL, NULL);
+	err = clfftEnqueueTransform(planHandle, CLFFT_BACKWARD, 1, &command_queue, 0, NULL, NULL, &freqInputBuffer, &timeInputBuffer, NULL);
+	err = clFinish(command_queue);
+	err = clEnqueueReadBuffer(command_queue, timeInputBuffer, CL_TRUE, 0, (inputLength + weightsLength) * sizeof(float), output, 0, NULL, NULL);
 
 	clReleaseMemObject(timeInputBuffer);
 	clReleaseMemObject(freqInputBuffer);
 	clReleaseMemObject(weightsBuffer);
 	clReleaseMemObject(freqWeightsBuffer);
+
 	err = clfftDestroyPlan(&planHandle);
 	clfftTeardown();
 }
