a6fa191dce9642fb00d26baf059c459c0e031f0a
Dhruv P <dpha900@aucklanduni.ac.nz>
2020-06-13 14:29:02 +1200

    Benchmark script

---
+++ benchmark.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from datetime import datetime
+
+executable = ".//x64//Release//SE751_filter.exe"
+filters = ["btd", "ocltd", "oclctd"];
+benchmark_files = "./benchmark_files/"
+weights = "weights.dat"
+inputs = []
+
+if not inputs:
+    for entry in os.scandir(benchmark_files):
+            if not entry.name.startswith('.') and not entry.name in weights and not entry.name in "output.dat":
+                inputs.append(entry.name)
+
+print(f"Benchmarking with:\n\tFilters: {filters}\n\tInputs: {benchmark_files}{inputs}\n\tWeights: {benchmark_files}{weights}\n")
+
+with open(f"benchmark_{datetime.now().strftime('%H-%M-%S')}.csv", 'a') as csv:
+    csv.write("filter,input,time\n")
+    for filter in filters:
+        for inputFile in inputs:
+            inputFile = "1KB.dat"
+            print(f"Running {filter} on {inputFile}")
+
+            p = subprocess.Popen([executable, filter, benchmark_files + inputFile, benchmark_files + weights, benchmark_files + "output.dat"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = p.communicate()
+            
+            if p.wait() != 0:
+                print("Last run had non-zero exit code")
+                exit()
+
+            for line in stdout.splitlines():
+                line = line.decode('ascii')
+                if line.startswith("Time: "):
+                    time = int(line[6:])
+                    csv.write(f"{filter},{inputFile},{time}\n")
